9652276ecb8d04fc630ac5ed2ab81b072fd156cf
dantasfelipe <raam0sfelipe@gmail.com>
2015-07-28 11:38:00 -0300

    adicionado views para criar/editar blogpost post dentro do site.

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-	return render(request, 'blogpost/post_list.html', {})
+	posts = Post.objects.filter(publicado_em__lte=timezone.now()).order_by('publicado_em')
+	return render(request, 'blogpost/post_list.html', {'posts':posts})
+
